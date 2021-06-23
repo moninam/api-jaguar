6b3d205c87280032f92a7fd1782090e60b777fbe
moninam <moninambri@gmail.com>
2021-06-23 04:15:46 -0500

    Cors

--- src/main/java/com/uady/apijaguar/controller/FileController.java
+++ src/main/java/com/uady/apijaguar/controller/FileController.java
@@ -8,8 +8,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -25,7 +27,7 @@ public class FileController {
 
     private Logger logger = LogManager.getLogger(this.getClass());
 
-    @GetMapping("/upload")
+    @PostMapping("/upload")
     public ResponseEntity<?> handleFileUpload(@RequestBody MultipartFile file,@RequestParam(name="ruta") String ruta) {
        
         boolean isUploaded = fileTransferService.uploadMultipartFile(file, ruta);
